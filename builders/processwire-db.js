48f06da62df321de83bc5838704c17a3316eee70
ryntab <soccershar18@gmail.com>
2023-09-19 15:08:33 -0400

    chore: :construction_worker: update pipeline

--- builders/processwire-db.js
+++ builders/processwire-db.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
 // Read environment variables
-const dbName = process.env.DB_NAME;
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
-const dbHost = process.env.DB_HOST;
+const env_DB_Name = process.env.DB_NAME;
+const env_DB_User = process.env.DB_USER;
+const env_DB_Password = process.env.DB_PASSWORD;
+const env_DB_Host = process.env.DB_HOST;
 
 // Path to your wp-config.php file
 const configFile = 'site/config.php';
@@ -12,12 +12,21 @@ const configFile = 'site/config.php';
 // Read the content of site/config.php
 const content = fs.readFileSync(configFile, 'utf8');
 
-// Replace the old credentials with new ones
+//Search Config file for DB_NAME, DB_USER, DB_PASSWORD, DB_HOST
+const db_name = content.match(/define\('DB_NAME', '(.*)'\);/)[1];
+const db_user = content.match(/define\('DB_USER', '(.*)'\);/)[1];
+const db_password = content.match(/define\('DB_PASSWORD', '(.*)'\);/)[1];
+const db_host = content.match(/define\('DB_HOST', '(.*)'\);/)[1];
+
+// Log the old credentials
+
+
+// Replace the old credentials with the new ones
 const updatedContent = content
-    .replace("define('DB_NAME', 'old_db_name');", `define('DB_NAME', 'test');`)
-    .replace("define('DB_USER', 'old_db_user');", `define('DB_USER', 'test');`)
-    .replace("define('DB_PASSWORD', 'old_db_password');", `define('DB_PASSWORD', 'test');`)
-    .replace("define('DB_HOST', 'old_db_host');", `define('DB_HOST', 'test');`);
+    .replace(db_name, env_DB_Name)
+    .replace(db_user, env_DB_User)
+    .replace(db_password, env_DB_Password)
+    .replace(db_host, env_DB_Host);
 
 // Write the updated content back to wp-config.php
 fs.writeFileSync(configFile, updatedContent);
